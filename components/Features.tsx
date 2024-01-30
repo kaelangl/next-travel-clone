67b958937ed87fbef9c070f375c25032b72ee092
Kaelan Lupton <kaelanlupton@gmail.com>
2024-01-30 01:56:06 -0500

    fixed stray features reference

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,4 +1,3 @@
-import { FEATURES } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
 
@@ -14,29 +13,6 @@ const Features = () => {
             height={1000}
             className="feature-phone"
           />
-        </div>
-
-        <div className="z-20 flex w-full flex-col lg:w-[60%]">
-          <div className='relative'>
-            <Image
-              src="/camp.svg"
-              alt="camp"
-              width={50}
-              height={50}
-              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
-            />
-            <h2 className="bold-40 lg:bold-64">Our Features</h2>
-          </div>
-          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
-            {FEATURES.map((feature) => (
-              <FeatureItem 
-                key={feature.title}
-                title={feature.title} 
-                icon={feature.icon}
-                description={feature.description}
-              />
-            ))}
-          </ul>
         </div>
       </div>
     </section>
